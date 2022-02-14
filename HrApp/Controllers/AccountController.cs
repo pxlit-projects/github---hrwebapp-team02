767d73c3a3f182ca954a10b8dc724dce3795e665
Emre <72259197+4Forcas@users.noreply.github.com>
2022-02-14 13:30:52 +0100

    username login, useremail login

--- HrApp/Controllers/AccountController.cs
+++ HrApp/Controllers/AccountController.cs
@@ -1,4 +1,5 @@
 ﻿using HrApp.ViewModels;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
@@ -9,9 +10,80 @@ namespace HrApp.Controllers
 {
     public class AccountController : Controller
     {
+        UserManager<IdentityUser> _userManager;
+        SignInManager<IdentityUser> _signInManager;
+
+        public AccountController(UserManager<IdentityUser> userManager, SignInManager<IdentityUser> signInManager)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+        }
+
+        [HttpGet]
+        public IActionResult LoginWithUserName()
+        {
+            return View();
+        }
+
+
+        [HttpPost]
+        public async Task<IActionResult> LoginWithUserName(LoginUsernameViewModel user)
+        {
+            if (ModelState.IsValid)
+            {
+                var identityuser = await _userManager.FindByNameAsync(user.UserName);
+                if (identityuser != null)
+                {
+                    var result = await _signInManager.PasswordSignInAsync(identityuser, user.Password, false, false);
+                    if (result.Succeeded)
+                    {
+                        return RedirectToAction("Index", "Home");
+                    }
+                    else
+                    {
+                        ModelState.AddModelError("", "Invalid login attempt.");
+                    }
+                }
+            }
+
+            return View(user);
+        }
+
+        [HttpGet]
+        public IActionResult LoginWithEmail()
+        {
+            return View();
+        }
+
+
+        [HttpPost]
+        public async Task<IActionResult> LoginWithEmail(LoginEmailViewModel user)
+        {
+            if (ModelState.IsValid)
+            {
+                var identityuser = await _userManager.FindByEmailAsync(user.Email);
+                if (identityuser != null)
+                {
+                    var result = await _signInManager.PasswordSignInAsync(identityuser, user.Password, false, false);
+                    if (result.Succeeded)
+                    {
+                        return RedirectToAction("Index", "Home");
+                    }
+                    else
+                    {
+                        ModelState.AddModelError("", "Invalid login attempt.");
+                    }
+                }
+            }
+
+            return View(user);
+        }
+
+
+
         public IActionResult Login()
         {
-            var login = "test";
+
             return View();
         }
         #region Register
@@ -21,8 +93,22 @@ namespace HrApp.Controllers
             return View();
         }
         [HttpPost]
-        public IActionResult Register(RegisterViewModel registerData)
+        public async Task<IActionResult> Register(RegisterViewModel registerData)
         {
+            if (ModelState.IsValid)
+            {
+
+                var identityUser = new IdentityUser() { UserName = registerData.UserName, Email = registerData.Email};
+                var result = await _userManager.CreateAsync(identityUser, registerData.Password);
+                if (result.Succeeded)
+                {
+                    return View("Login");
+                }
+                else
+                {
+                    
+                }
+            }
             return View();
         }
         #endregion
